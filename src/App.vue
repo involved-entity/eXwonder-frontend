2f5d98cd0dc1057521245419cbe63e853c2d92df
waflawe <popejpivaoleg@gmail.com>
2024-10-12 17:08:08 +0900

    Add basic navbar template.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <router-view></router-view>
+  <div class="flex">
+    <div class="w-1/5">
+      <the-navbar></the-navbar>
+    </div>
+    <div class="w-3/5">
+      <router-view></router-view>
+    </div>
+    <div class="w-1/5">
+
+    </div>
+  </div>
 </template>
+
+<script>
+import TheNavbar from "./components/TheNavbar.vue";
+
+export default {
+  components: {TheNavbar}
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/SingUpView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
+    {path: '/', name: 'home', component: HomeView},
     {path: '/login/', name: 'login', component: LoginView},
     {path: '/sing-up/', name: 'sing-up', component: RegisterView}
 ]
 
 export const router = createRouter({
     routes,
-    history: createWebHistory()
+    history: createWebHistory(),
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'active'
 })
