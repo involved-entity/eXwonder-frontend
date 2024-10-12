830b1c905666223898c8fd97000c96c226d86c5d
waflawe <popejpivaoleg@gmail.com>
2024-10-12 17:30:08 +0900

    Add basic guard for login require routes.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex">
-    <div class="w-1/5">
+    <div class="w-1/5" v-if="authenticationStore.isAuth">
       <the-navbar></the-navbar>
     </div>
-    <div class="w-3/5">
+    <div :class="{'w-3/5': authenticationStore.isAuth, 'w-full': !authenticationStore.isAuth}">
       <router-view></router-view>
     </div>
-    <div class="w-1/5">
+    <div class="w-1/5" v-if="authenticationStore.isAuth">
 
     </div>
   </div>
@@ -14,8 +14,13 @@
 
 <script>
 import TheNavbar from "./components/TheNavbar.vue";
+import {mapStores} from 'pinia'
+import {useAuthenticationStore} from './stores/authenticationStore.js'
 
 export default {
-  components: {TheNavbar}
+  components: {TheNavbar},
+  computed: {
+    ...mapStores(useAuthenticationStore)
+  }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,33 @@ import { createWebHistory, createRouter } from 'vue-router'
 import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/SingUpView.vue";
 import HomeView from "../views/HomeView.vue";
+import {useAuthenticationStore} from '../stores/authenticationStore.js'
 
 const routes = [
-    {path: '/', name: 'home', component: HomeView},
-    {path: '/login/', name: 'login', component: LoginView},
-    {path: '/sing-up/', name: 'sing-up', component: RegisterView}
+    {path: '/', name: 'home', component: HomeView, meta: {requireLogin: true}},
+    {path: '/login/', name: 'login', component: LoginView, meta: {requireNoLogin: true}},
+    {path: '/sing-up/', name: 'sing-up', component: RegisterView, meta: {requireNoLogin: true}}
 ]
 
-export const router = createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory(),
     linkActiveClass: 'active',
     linkExactActiveClass: 'active'
 })
+
+router.beforeEach((to, from, next) => {
+    const authenticationStore = useAuthenticationStore()
+    if (to.meta.requireLogin) {
+        if (!authenticationStore.isAuth) {
+            return next('/login/')
+        }
+    } else if (to.meta.requireNoLogin) {
+        if (authenticationStore.isAuth) {
+            return next('/')
+        }
+    }
+    return next()
+})
+
+export default router
