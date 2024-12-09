cfbabd8a49da741cf7f1d58ef7830810977ef2c0
waflawe <popejpivaoleg@gmail.com>
2024-12-10 04:46:20 +0900

    Add pagination by scrolling in saved posts page

--- src/views/SavedPostsView.vue
+++ src/views/SavedPostsView.vue
@@ -14,7 +14,14 @@
         <div class="py-10" v-if="loading">
           <div class="loader mx-auto"></div>
         </div>
-        <app-posts-grid :posts="saved" v-if="!loading" />
+        <app-posts-grid
+          :posts="saved"
+          @updatePostsScroll="getPostsNextPage"
+          v-if="!loading"
+        />
+        <div class="py-10" v-if="loadingNextPage">
+          <div class="loader mx-auto"></div>
+        </div>
       </div>
     </div>
   </main>
@@ -32,24 +39,34 @@ export default {
     return {
       loading: false,
       saved: [] as Array<IPost>,
+      loadingNextPage: false,
+      currentPage: undefined as number | undefined,
     };
   },
   methods: {
+    formatSavedPosts: (post: IPost) => {
+      post.post.likes_count = post.likes_count;
+      post.post.comments_count = post.comments_count;
+      post.post.is_liked = post.is_liked;
+      post.post.is_commented = post.is_commented;
+      post.post.is_saved = post.is_saved;
+      return post.post;
+    },
     async getPostsSaved() {
       this.loading = true;
       const response: IResponse = await this.postsStore.getSavedPosts();
-      this.saved = response.data.results;
-
-      this.saved = this.saved.map((post: IPost) => {
-        post.post.likes_count = post.likes_count;
-        post.post.comments_count = post.comments_count;
-        post.post.is_liked = post.is_liked;
-        post.post.is_commented = post.is_commented;
-        post.post.is_saved = post.is_saved;
-        return post.post;
-      });
-
+      this.saved = response.data.results.map(this.formatSavedPosts);
+      this.currentPage = response.data.next ? 2 : undefined;
       this.loading = false;
+    },
+    async getPostsNextPage() {
+      if (this.currentPage) {
+        this.loadingNextPage = true;
+        const { data } = await this.postsStore.getSavedPosts(this.currentPage);
+        this.saved.push(...data.results.map(this.formatSavedPosts));
+        this.currentPage = data.next ? this.currentPage + 1 : undefined;
+        this.loadingNextPage = false;
+      }
     },
   },
   async mounted() {
