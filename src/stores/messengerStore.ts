44c46d2167cf851eece59281ecefc597e7ccb37e
waflawe <popejpivaoleg@gmail.com>
2024-12-16 14:50:36 +0900

    Add basic action to init messenger store

--- src/stores/messengerStore.ts
+++ src/stores/messengerStore.ts
@@ -1,5 +1,7 @@
 import {defineStore} from "pinia";
+import {MessengerUrl} from "../settings.ts";
 import {IMessage, IChat} from "../types/stores";
+import {useAuthenticationStore} from "../stores/authenticationStore.ts";
 
 export const useMessengerStore = defineStore("messenger", {
     state() {
@@ -7,6 +9,41 @@ export const useMessengerStore = defineStore("messenger", {
             messages: [] as Array<IMessage>,
             chats: [] as Array<IChat>,
             socket: undefined as WebSocket | undefined
+        }
+    },
+    actions: {
+        onSocketOpen() {
+            const authStore = useAuthenticationStore();
+            let data = JSON.stringify({
+                type: "authenticate",
+                token: authStore.token,
+                user_id: authStore.user.id,
+            });
+            this.socket!.send(data);
+            data = JSON.stringify({
+                type: "connect_to_chats",
+            });
+            this.socket!.send(data);
+        },
+        onSocketMessage(event: object) {
+            const data = JSON.parse(event.data);
+            if (data.type === "connect_to_chats") {
+                this.chats = data.payload
+            }
+        },
+        initMessenger() {
+            const authStore = useAuthenticationStore();
+            if (authStore.isAuth) {
+                this.socket = new WebSocket(MessengerUrl)
+                this.socket.onopen = this.onSocketOpen
+                this.socket.onmessage = this.onSocketMessage;
+                this.socket.onclose = function (event) {
+                    console.log("Соединение закрыто:", event);
+                    if (event.code) {
+                        console.error("Код закрытия:", event.code);
+                    }
+                };
+            }
         }
     }
 })
